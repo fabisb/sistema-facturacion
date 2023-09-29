8418506934ea8e6b7182787a9458531ddadcce38
Fabian Silva <silvabravofabian@gmail.com>
2023-09-29 16:00:45 -0400

    creada pantalla agregar

--- back/database/facturacion.sql
+++ back/database/facturacion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-09-2023 a las 19:37:34
+-- Tiempo de generación: 27-09-2023 a las 23:50:57
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,6 +24,61 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `cantidad_producto`
+--
+
+CREATE TABLE `cantidad_producto` (
+  `id` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` decimal(7,2) NOT NULL,
+  `minimo` decimal(7,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `cedula` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL,
+  `telefono` varchar(50) NOT NULL,
+  `direccion` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_factura`
+--
+
+CREATE TABLE `detalle_factura` (
+  `id` int(11) NOT NULL,
+  `id_producto` int(20) NOT NULL,
+  `cantidad` int(10) NOT NULL,
+  `id_factura` int(20) NOT NULL,
+  `precio` decimal(12,2) NOT NULL,
+  `total` decimal(12,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE `factura` (
+  `id` int(11) NOT NULL,
+  `monto` decimal(11,2) NOT NULL,
+  `moneda` varchar(40) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `cliente` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `niveles_usuario`
 --
 
@@ -40,6 +95,18 @@ CREATE TABLE `niveles_usuario` (
 INSERT INTO `niveles_usuario` (`id`, `nombre`, `descripcion`) VALUES
 (1, 'admin', 'Mayor nivel de usuario'),
 (2, 'cajero', 'Menor nivel de usuario');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(150) NOT NULL,
+  `tipo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -67,9 +134,44 @@ INSERT INTO `users` (`id`, `username`, `password`, `nivel`) VALUES
 --
 
 --
+-- Indices de la tabla `cantidad_producto`
+--
+ALTER TABLE `cantidad_producto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_id_producto` (`id_producto`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`cedula`);
+
+--
+-- Indices de la tabla `detalle_factura`
+--
+ALTER TABLE `detalle_factura`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_id_factura` (`id_factura`),
+  ADD KEY `FK_id_producto_detalle` (`id_producto`);
+
+--
+-- Indices de la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_id_usuario` (`id_usuario`),
+  ADD KEY `FK_cliente` (`cliente`);
+
+--
 -- Indices de la tabla `niveles_usuario`
 --
 ALTER TABLE `niveles_usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -84,10 +186,34 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `cantidad_producto`
+--
+ALTER TABLE `cantidad_producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_factura`
+--
+ALTER TABLE `detalle_factura`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `factura`
+--
+ALTER TABLE `factura`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `niveles_usuario`
 --
 ALTER TABLE `niveles_usuario`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
@@ -98,6 +224,26 @@ ALTER TABLE `users`
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `cantidad_producto`
+--
+ALTER TABLE `cantidad_producto`
+  ADD CONSTRAINT `FK_id_producto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `detalle_factura`
+--
+ALTER TABLE `detalle_factura`
+  ADD CONSTRAINT `FK_id_factura` FOREIGN KEY (`id_factura`) REFERENCES `factura` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_id_producto_detalle` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id`);
+
+--
+-- Filtros para la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD CONSTRAINT `FK_cliente` FOREIGN KEY (`cliente`) REFERENCES `cliente` (`cedula`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `users`
