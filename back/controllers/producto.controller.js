68081c77cffdd873a44eb28e58780b6a66596505
Fabian Silva <silvabravofabian@gmail.com>
2023-10-03 12:09:48 -0400

    proceso de creacion de modal para obtener id de producto

--- back/controllers/producto.controller.js
+++ back/controllers/producto.controller.js
@@ -1,15 +1,15 @@
 import { pool } from "../database/db.js";
 
-export const agregarController = async (req, res, next) => {
+export const agregarController = async (req, res) => {
   console.log("AgregarController");
   const { nombre, tipo, cantidad } = req.body;
 
   if (nombre == "" || cantidad == "" || cantidad == 0 || tipo == "") {
     console.log("Error falta algun dato");
-    return res.status(400).json({ mensaje: "Error falta algun dato" });
+    return await res.status(400).json({ mensaje: "Error falta algun dato" });
   } else if (isNaN(cantidad)) {
     console.log("Error tipo de dato");
-    return res.status(400).json({ mensaje: "Error tipo de dato" });
+    return await res.status(400).json({ mensaje: "Error tipo de dato" });
   }
   const [existencia] = await pool.execute(
     "SELECT nombre, tipo FROM productos WHERE nombre = ? AND tipo = ?",
@@ -17,7 +17,7 @@ export const agregarController = async (req, res, next) => {
   );
   console.log("🚀 ~ file: producto.controller.js:18 ~ agregarController ~ existencia:", existencia);
   if (existencia.length > 0) {
-    return res
+    return await res
       .status(405)
       .json({ mensaje: "Error ya se encontro un producto de igual nombre y tipo" });
   } else {
@@ -33,6 +33,53 @@ export const agregarController = async (req, res, next) => {
       insertado.insertId,
       cantidad,
     ]);
-    return res.status(200).json({ mensaje: "Insertado exitoso" });
+    return await res.status(200).json({ mensaje: "Insertado exitoso" });
   }
 };
+
+export const editarController = async (req, res) => {
+  console.log("editarController");
+  const { nombre, tipo, cantidad } = req.body;
+
+  if (nombre == "" || cantidad == "" || cantidad == 0 || tipo == "") {
+    console.log("Error falta algun dato");
+    return await res.status(400).json({ mensaje: "Error falta algun dato" });
+  } else if (isNaN(cantidad)) {
+    console.log("Error tipo de dato");
+    return await res.status(400).json({ mensaje: "Error tipo de dato" });
+  }
+  const [existencia] = await pool.execute(
+    "SELECT nombre, tipo FROM productos WHERE nombre = ? AND tipo = ?",
+    [nombre, tipo]
+  );
+  console.log("🚀 ~ file: producto.controller.js:18 ~ agregarController ~ existencia:", existencia);
+  if (existencia.length > 0) {
+    return await res
+      .status(405)
+      .json({ mensaje: "Error ya se encontro un producto de igual nombre y tipo" });
+  } else {
+    const [actualizado] = await pool.execute("UPDATE FROM productos SET nombre = ? tipo = ? WHERE id = ?", [
+      nombre,
+      tipo,
+      idExistente
+    ]);
+    console.log(
+      "🚀 ~ file: producto.controller.js:28 ~ agregarController ~ insertado:",
+      actualizado.insertId
+    );
+    await pool.execute("INSERT INTO cantidad_producto (id_producto, cantidad) VALUES (?,?)", [
+      actualizado.insertId,
+      cantidad,
+    ]);
+    return await res.status(200).json({ mensaje: "Actualizado exitoso" });
+  }
+};
+
+export const idProductoController = async (req,res)=>{
+  console.log('idProductoController')
+  const producto = req.query;
+  console.log("🚀 ~ file: producto.controller.js:80 ~ idProducto ~  id:",  producto);
+  const [id] = await pool.execute('SELECT id FROM productos WHERE nombre = ? AND tipo = ?',[producto.nombre,producto.tipo])
+  console.log("🚀 ~ file: producto.controller.js:83 ~ idProductoController ~ id:", id)
+  return await res.status(200).json(id); 
+} 
--- back/routes/producto.routes.js
+++ back/routes/producto.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { verifyToken } from "../controllers/login.controller.js";
-import { agregarController } from "../controllers/producto.controller.js";
+import { agregarController, editarController, idProductoController } from "../controllers/producto.controller.js";
 var router = express.Router();
 
 /* POST users listing. */
 router.post("/agregar",verifyToken,agregarController );
+router.patch("/editar",verifyToken,editarController );
 
+router.get('/idProducto',verifyToken,idProductoController)
 export default router;
