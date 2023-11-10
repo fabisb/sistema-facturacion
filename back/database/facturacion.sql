76ad68b87c70618bffa8df208e067ebcbb6ffc1c
Fabian Silva <silvabravofabian@gmail.com>
2023-11-09 22:46:57 -0400

    bdd

--- back/database/facturacion.sql
+++ back/database/facturacion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-11-2023 a las 22:35:19
+-- Tiempo de generación: 10-11-2023 a las 03:46:24
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
